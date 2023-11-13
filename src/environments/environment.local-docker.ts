1111f9bdc27b614b0e0b0c23a9b68ff04ebcc30b
morriswa <william@morriswa.org>
2023-11-12 22:53:12 -0600

    run online

---
+++ src/environments/environment.local-docker.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import package_settings from 'package.json';
+
+export const environment = {
+  production: false,
+  api : "http://host.docker.internal:8081/",
+  securepath : 'v0/',
+  auth : {
+    "domain" : "dev-9deub659.us.auth0.com",
+    "clientId" : "Wi9msBPJr0pr56mPWJy4FuSWwQKFVmAA",
+    "audience" : "http://127.0.0.1:8081/v0",
+    "scopes": "openid email profile"
+  },
+  app_version: package_settings.version,
+};
