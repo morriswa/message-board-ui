eb89cfda8effced3ecfa91a60e76017583685088
morriswa <william@morriswa.org>
2023-11-28 13:52:06 -0600

    fix edit community bug and extern validator msg config

--- src/app/components/community/edit-community/edit-community.component.ts
+++ src/app/components/community/edit-community/edit-community.component.ts
@@ -1,12 +1,12 @@
 import {Component, EventEmitter} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {Router} from "@angular/router";
 import {MessageBoardClientService} from "../../../service/message-board-client.service";
 import {UploadImageRequest} from "../../../interface/upload-image-request";
-import {map, of, switchMap} from "rxjs";
+import {Observable, of, switchMap, tap} from "rxjs";
 import {ValidatorFactory} from "../../../service/validator.factory";
 import {FormControl} from "@angular/forms";
-import {CommunityResponse} from "../../../interface/community";
 import {CurrentCommunityService} from "../current-community.service";
+import {PreferencesService} from "../../../service/preferences.service";
 
 @Component({
   selector: 'app-edit-community',
@@ -21,25 +21,23 @@ export class EditCommunityComponent {
   communityLocatorForm: FormControl;
   communityDisplayNameForm: FormControl;
 
-  // get communityInfo():CommunityResponse {
-  //   return this.currentCommunity.community;
-  // }
-
   stagedContentForUpload: {
    icon?: UploadImageRequest
    banner?: UploadImageRequest
   } = {};
 
   resetEventEmitter: EventEmitter<any> = new EventEmitter<any>();
+  preferences: any;
 
   constructor(validatorFactory: ValidatorFactory,
-              private activeRoute: ActivatedRoute,
+              currentPrefs: PreferencesService,
               private router: Router,
               private messageBoardService: MessageBoardClientService,
               public currentCommunity: CurrentCommunityService,
   ) {
     this.communityLocatorForm = validatorFactory.getCommunityRefForm();
     this.communityDisplayNameForm = validatorFactory.getCommunityDisplayNameForm();
+    this.preferences = currentPrefs.preferences;
   }
 
   updateCommunity() {
@@ -48,19 +46,18 @@ export class EditCommunityComponent {
 
     const newRef = this.communityLocatorForm.value!;
 
-    this.messageBoardService.editCommunityAttributes(
-      this.currentCommunity.id,
-      newRef,
-      this.communityDisplayNameForm.value)
+    of('update community triggered!')
       .pipe(
+        switchMap(()=>this.updateCommunityAttrs()),
         switchMap(()=>this.updateCommunityBanner()),
         switchMap(()=>this.updateCommunityIcon()),
       )
       .subscribe({
       next: ()=>{
+        this.resetEventEmitter.emit();
+
         let nav = newRef? newRef : this.currentCommunity.locator
-        this.communityLocatorForm.reset()
-        this.communityDisplayNameForm.reset()
+
         this.router.navigateByUrl('/',{ skipLocationChange: true})
           .then(()=>this.router.navigate(['/community',nav]));
       }, error: (response:any) =>{
@@ -75,33 +72,41 @@ export class EditCommunityComponent {
     })
   }
 
-  public updateCommunityBanner() {
-    if (this.stagedContentForUpload.banner) {
-
-      return this.messageBoardService.updateCommunityBanner(
+  updateCommunityAttrs(): Observable<any> {
+    // if either form has a value
+    if ((this.communityDisplayNameForm.value || this.communityLocatorForm.value)
+      // and the value(s) are valid
+      && !(this.communityDisplayNameForm.invalid || this.communityLocatorForm.invalid))
+        // then call update endpoint
+        return this.messageBoardService.editCommunityAttributes(
           this.currentCommunity.id,
-          this.stagedContentForUpload.banner!)
-        .pipe(map(()=> {
-            this.resetEventEmitter.emit();
-            return 'updated community banner'
-          }));
-    }
+          this.communityLocatorForm.value,
+          this.communityDisplayNameForm.value).pipe(tap(()=>{
+            this.communityLocatorForm.reset();
+            this.communityDisplayNameForm.reset();
+          }
+        ));
+
+    // else skip
+    else return of('no values to update!');
+  }
+
+  public updateCommunityBanner(): Observable<any> {
+    if (this.stagedContentForUpload.banner)
+      return this.messageBoardService
+        .updateCommunityBanner(
+          this.currentCommunity.id,
+          this.stagedContentForUpload.banner!);
 
     return of('did not update banner')
   }
-  public updateCommunityIcon() {
 
-    if (this.stagedContentForUpload.icon) {
-
-      return this.messageBoardService.updateCommunityIcon(
+  public updateCommunityIcon(): Observable<any> {
+    if (this.stagedContentForUpload.icon)
+      return this.messageBoardService
+        .updateCommunityIcon(
           this.currentCommunity.id,
-          this.stagedContentForUpload.icon!)
-        .pipe(map(()=> {
-          this.resetEventEmitter.emit();
-          return 'updated community icon'
-        }));
-
-    }
+          this.stagedContentForUpload.icon!);
 
     return of('did not update icon')
   }
